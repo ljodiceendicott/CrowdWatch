3adde4d7d3ab59c5cdb05dc0f43f3caef0fd3cb3
ljodice <>
2022-11-15 19:57:33 -0500

    Added Database solution

--- Website/Inventory Website/index.js
+++ Website/Inventory Website/index.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const app = express();
-const datastore = require('nedb');
+const Datastore = require('nedb');
 
 app.listen(3000, ()=> console.log("Listening at 3000"));
 app.use(express.static('public'));
 app.use(express.json())//can add different options in the parameter, will do this later
 
-const database = new datastore('database.db')
+const database = new Datastore('database.db')
 database.loadDatabase();
 
+database.insert({action: 'Login', date : Date.now()})
+
 app.post('/endpoint', (request, response) => {
-    console.log(request.body);
+    const data = request.body;
+    data.timestamp = Date.now();
+    database.insert(data);
     response.json({
         status : 'successful'
     })
+});
+
+app.get('/logs', (request,response) => {
+    database.insert({action: 'Login', date : Date.now()})
+    database.find({},(err, data) =>{
+        if(err){
+            response.end();
+            return;
+        }
+        response.json(data);
+        console.log(data);
+    });
 });
