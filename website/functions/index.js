58a6d560d436a8b493633fa5e1bb4a8247b69cbe
ljodice <>
2023-03-29 07:38:37 -0400

    website JS work

--- website/functions/index.js
+++ website/functions/index.js
@@ -1,7 +1,40 @@
 const functions = require("firebase-functions");
 // const firebase = require("firebase");
 const express = require("express");
+const { initializeApp } = require("firebase-admin");
+const { getDatabase } = require("firebase-admin/database");
 const app = express();
+const firebaseConfig = {
+  apiKey: "AIzaSyCH0gyZ9RUUdHsLHEM7hYhyYHqmQoVJsKY",
+  authDomain: "crowdwatch-ce5b8.firebaseapp.com",
+  projectId: "crowdwatch-ce5b8",
+  storageBucket: "crowdwatch-ce5b8.appspot.com",
+  messagingSenderId: "267648106581",
+  appId: "1:267648106581:web:3a11c1688282768f4cd4dc",
+  measurementId: "G-9GVLV3ZJNE",
+  databaseURL: "https://crowdwatch-ce5b8-default-rtdb.firebaseio.com/",
+};
+const appinit = initializeApp(firebaseConfig);
+const db = getDatabase(appinit);
+var database = firebase.database();
+
+//Connecting to the Emulators for testing
+if (location.hostname === "localhost") {
+  // Point to the RTDB emulator running on localhost.
+  firebase.functions.useEmulator("localhost", 4000);
+  db.useEmulator("localhost", 50);
+}
+
+// Attach an asynchronous callback to read the data at our posts reference
+ref.on(
+  "value",
+  (snapshot) => {
+    console.log(snapshot.val());
+  },
+  (errorObject) => {
+    console.log("The read failed: " + errorObject.name);
+  }
+);
 // const fs = require("fs");
 // const data = fs.readFileSync("data.json");
 // const words = JSON.parse(data);
