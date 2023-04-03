b0c895bb1b75c1564f5d41ddee005f1d3120cc23
ljodice <>
2023-04-03 09:23:24 -0400

    http requests from python

--- python/actions.py
+++ python/actions.py
@@ -1,13 +1,12 @@
-#Luke Jodice
-#Actions.py
-#This file is where common functions are located. These actions are used throughout the other files 
-#they are in one location as there are different uses for them and they are very generic allowing for 
-#them to be used for different pourposes 
+# Luke Jodice
+# Actions.py
+# This file is where common functions are located. These actions are used throughout the other files
+# they are in one location as there are different uses for them and they are very generic allowing for
+# them to be used for different pourposes
 
-#checklist
+# checklist
 # [X] spreadsheet
 # [X] set to json
-# [ ] SQL
 # [X] Read from Arduino
 # [ ] Networking-GET/PUT/POST
 
@@ -18,132 +17,168 @@ from openpyxl import Workbook, load_workbook
 import requests
 
 # ////////////////////////////////////////////////////////////////////////////////////////
-#Json 
+# Json
 # ////////////////////////////////////////////////////////////////////////////////////////
 
-#class needed for object to json transfer
+# class needed for object to json transfer
 class expJson(object):
     def __init__(self, object):
         self.account = object
-        
-        
-#returns data in form of json for a complex object
+
+
+# returns data in form of json for a complex object
 def serialize(object):
     data = json.dumps(object, default=lambda o: o.__dict__)
     return data
 
-#Returns the data that is found in Json File that is for the specific business returning the business OBJ
-# @param - name - Name the json file that is being looked for in user files 
+
+# Returns the data that is found in Json File that is for the specific business returning the business OBJ
+# @param - name - Name the json file that is being looked for in user files
 def read_from_Json(name):
-    file = open('../UserFiles/'+name+'.json')
+    file = open("../UserFiles/" + name + ".json")
     data = json.load(file)
     file.close()
     return data
-#This is a Use case from reading in from json
-#------how to access the location name and adding the count member and making it zero------
+
+
+# This is a Use case from reading in from json
+# ------how to access the location name and adding the count member and making it zero------
 # data = read_from_Json("Test")
 # for i in data['account']['locations']:
 #     name = i['name']
 #     i['count'] = 0
 
 
-#Writes the current Data to the json file. that will be updated
+# Writes the current Data to the json file. that will be updated
 # @param data - data to be written to the file -Type(Json Object)
-# @param name - name of the file to be written -Type(String)  
+# @param name - name of the file to be written -Type(String)
 def write_to_Json(data, name):
-    with open(name+".json", 'w') as outfile:
+    with open(name + ".json", "w") as outfile:
         outfile.write(data)
-#Use Case
+
+
+# Use Case
 # adjusting the count of a location then writing to a file
-    # data = read_from_Json('paddy')
-    # locations = data['account']['locations']
-    # loc1= locations[0]
-    # loc1count = locations[0]['count']+1
-    # loc1.update({"count": loc1count})
-    # write_to_Json(serialize(data),'paddy')
+# data = read_from_Json('paddy')
+# locations = data['account']['locations']
+# loc1= locations[0]
+# loc1count = locations[0]['count']+1
+# loc1.update({"count": loc1count})
+# write_to_Json(serialize(data),'paddy')
 
 
-#Returns the Date as a string of numbers 
+# Returns the Date as a string of numbers
 # EX.) 2301311802
-#order of num => Year-Month-Day-Hour-Minute
+# order of num => Year-Month-Day-Hour-Minute
 def getDate():
     now = datetime.now()
-    dt_string= now.strftime("%y-%m-%d %H:%M")
+    dt_string = now.strftime("%y-%m-%d %H:%M")
     return dt_string
 
+
 # ////////////////////////////////////////////////////////////////////////////////////////
-#Spreadsheet Code
+# Spreadsheet Code
 # ////////////////////////////////////////////////////////////////////////////////////////
 
 
-#Returns instance of Workbook
-#@param name - Name of the business that you are tryin to get access to their spreadsheet
+# Returns instance of Workbook
+# @param name - Name of the business that you are tryin to get access to their spreadsheet
 def workbookInstance(name):
-    workbook = load_workbook('../UserFiles/'+name+'businessAnalysis.xlsx')
-    print('instance made')
+    workbook = load_workbook("../UserFiles/" + name + "businessAnalysis.xlsx")
+    print("instance made")
     return workbook
 
-#adds data to spreadsheet
-#@param fn - File name that will be written to
-#@param data- data to be added. How it should be formatted: [get_Date(),count1,count2,count3, ...., countn]
+
+# adds data to spreadsheet
+# @param fn - File name that will be written to
+# @param data- data to be added. How it should be formatted: [get_Date(),count1,count2,count3, ...., countn]
 def spreadsheet_Data_Add(fn, data):
     workbook = workbookInstance(fn)
-    sheet = workbook.active 
+    sheet = workbook.active
     column = nextColumn(fn, workbook)
     print(column)
-    for i in range(0,len(data)): #Loops through all of the data that is given from update
+    for i in range(
+        0, len(data)
+    ):  # Loops through all of the data that is given from update
         print(data[i])
-        cell = sheet.cell(row = i+1, column = column)
+        cell = sheet.cell(row=i + 1, column=column)
         print(cell.value)
         cell.value = data[i]
-    workbook.save('../UserFiles/'+fn+'businessAnalysis.xlsx')
+    workbook.save("../UserFiles/" + fn + "businessAnalysis.xlsx")
 
-#Returns the index of the next avaliable column to be written to
-#@param fn - File name that will be written to
-#@param wb - Instance of the Workbook
+
+# Returns the index of the next avaliable column to be written to
+# @param fn - File name that will be written to
+# @param wb - Instance of the Workbook
 def nextColumn(fn, wb):
     workbook = wb
     sheet = workbook.active
-    #loops through all of the columns known
-    for col in range(1, sheet.max_column+2):
-        #if the header is not there then the column can be written to
-        if sheet.cell(row = 1, column = col).value == None or sheet.cell(row=1, column = col).value == "": 
+    # loops through all of the columns known
+    for col in range(1, sheet.max_column + 2):
+        # if the header is not there then the column can be written to
+        if (
+            sheet.cell(row=1, column=col).value == None
+            or sheet.cell(row=1, column=col).value == ""
+        ):
             print("found empty spot @", col)
             return col
 
+
 # ////////////////////////////////////////////////////////////////////////////////////////
-#SQL DATABASE
+# SQL DATABASE
 # ////////////////////////////////////////////////////////////////////////////////////////
 
 
-#Returns SQL DB object as a Tuple
-#Tuple[0] - sqliteConnection
-#Tuple[1] - Cursor obj
-#@param name - name of the sql db
+# Returns SQL DB object as a Tuple
+# Tuple[0] - sqliteConnection
+# Tuple[1] - Cursor obj
+# @param name - name of the sql db
 def databaseConnect(name):
-    sqliteConnection = sqlite3.connect("../UserFiles/"+name +'SQL.db')
+    sqliteConnection = sqlite3.connect("../UserFiles/" + name + "SQL.db")
     cursor = sqliteConnection.cursor()
-    return sqliteConnection,cursor
+    return sqliteConnection, cursor
+
 
 def EnterDatabase(obj, data, table):
     db = databaseConnect(table)
-    for i in range(len(data-2)):
+    for i in range(len(data - 2)):
         idx = i + 2
-        print()  
-        
-# ////////////////////////////////////////////////////////////////////////////////////////  
-#Networking 
+        print()
+
+
+# ////////////////////////////////////////////////////////////////////////////////////////
+# Networking
 # ////////////////////////////////////////////////////////////////////////////////////////
 
 
 def getRequest():
     print()
 
-def putRequest():
+
+def putRequest(location):
+    r = requests.put("http://localhost:3000/putReq", data={})
+    # success code
+    print(r)
+    # Content of Req
     print()
-    
+
+
 def postRequest():
-    print()
+    # URL for testing
+    url = "http://localhost:3000/test"
+    # URL for live -- To be added
+    data = {"status": "connected", "date": getDate()}
+    postlog(data)
+    x = requests.post(url, json=data)
+
+    print(x.text)
+
+
+def postlog(data):
+    url = "http://localhost:3000/logs"
+    data = data
+    x = requests.post(url, json=data)
+    print(x.text)
 
 
 ##~~~Testing Area~~~~##
@@ -159,12 +194,13 @@ def postRequest():
 
 
 ##~~Testing Area End ~~~~~##
+
+postRequest()
 # nextColumn('paddy')
 # Sample spreadsheet_Data_Add(NAME_OF_BUS, DATA_FROM_BUS)
-#Sample call: spreadsheet_Data_Add(account.name, account.data)
-# spreadsheet_Data_Add('paddy',[get_Date(),1,2,3]) 
+# Sample call: spreadsheet_Data_Add(account.name, account.data)
+# spreadsheet_Data_Add('paddy',[get_Date(),1,2,3])
 # print(serialize(data))
-
 
 
 # file = open("..\Website\PeopleWatchSite\public\data\data.json.json")
@@ -174,8 +210,8 @@ def postRequest():
 # dataUpdate = dataUpdate + 3
 # dataloc.update({"amount":dataUpdate})
 # write_to_Json(serialize(data), '..\Website\PeopleWatchSite\public\data\data.json')
-    # locations = data['account']['locations']
-    # loc1= locations[0]
-    # loc1count = locations[0]['count']+1
-    # loc1.update({"count": loc1count})
-    # write_to_Json(serialize(data),'./UserFiles/paddy')
+# locations = data['account']['locations']
+# loc1= locations[0]
+# loc1count = locations[0]['count']+1
+# loc1.update({"count": loc1count})
+# write_to_Json(serialize(data),'./UserFiles/paddy')
--- website/index.js
+++ website/index.js
@@ -1,22 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const Datastore = require('nedb');
-const mysql = require('mysql')
-const fs = require('fs');
-var data = fs.readFileSync('data.json');
+const Datastore = require("nedb");
+const mysql = require("mysql");
+const fs = require("fs");
+var data = fs.readFileSync("data.json");
 var words = JSON.parse(data);
 // console.log(words)
+var DataChange = false;
 
+app.listen(3000, () => console.log("Connecting to Port: 3000"));
+app.use(express.static("public"));
+app.use(express.json()); //can add different options in the parameter, will do this later
 
-app.listen(3000, ()=> console.log("Connecting to Port: 3000"));
-app.use(express.static('public'));
-app.use(express.json())//can add different options in the parameter, will do this later
-
-
-const database = new Datastore('database.db')
+const database = new Datastore("database.db");
 database.loadDatabase();
 
-database.insert({action: 'Login', date : Date.now()})
+database.insert({ action: "Login", date: Date.now() });
 
 // var con = mysql.createConnection({
 //     host:"localhost",
@@ -29,34 +28,46 @@ database.insert({action: 'Login', date : Date.now()})
 //     console.log("Connected!");
 // })
 
-app.post('/endpoint', (request, response) => {
-    const data = request.body;
-    data.timestamp = Date.now();
-    database.insert(data);
-    response.json({
-        status : 'successful'
-    })
+app.post("/endpoint", (request, response) => {
+  const data = request.body;
+  data.timestamp = Date.now();
+  database.insert(data);
+  response.json({
+    status: "successful",
+  });
 });
 
-app.get('/currentvalues', (req, res) => {
-    res.json(words)
-})
+app.post("/test", (request, response) => {
+  const data = request.body;
+  console.log(data);
+  response.json({
+    status: "IT  Worked",
+  });
+});
 
-app.patch("/DataChange/:location/:number?", (request, response) =>{
+app.post("/logs", (req, resp) => {
+  const data = req.body;
+  database.insert(data);
+  resp.json({
+    status: "100 Data sent to DB",
+  });
+});
+app.get("/currentvalues", (req, res) => {
+  res.json(words);
+});
 
-})
+app.patch("/DataChange/:location/:number?", (request, response) => {});
 
-
-app.get('/logs', (request,response) => {
-    database.insert({action: 'Login', date : Date.now()})
-    database.find({},(err, data) =>{
-        if(err){
-            response.end();
-            return;
-        }
-        response.json(data);
-        console.log(data);
-    });
+app.get("/logs", (request, response) => {
+  database.insert({ action: "Login", date: Date.now() });
+  database.find({}, (err, data) => {
+    if (err) {
+      response.end();
+      return;
+    }
+    response.json(data);
+    console.log(data);
+  });
 });
 
 // router.get('/', function(req, res, next) {
